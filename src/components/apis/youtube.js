f33d53eed0981bb7bb4fc6aa29c6713a6b5e5c96
Tyler Vannoy <61971482+TylerJVannoy@users.noreply.github.com>
2021-08-13 14:09:50 -0700

    changed key

--- src/components/apis/youtube.js
+++ src/components/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const KEY = "AIzaSyDr4v7Mwku1jtwCSLAOYyqbiKnWmQ_gyb4";
+const KEY = "AIzaSyDUUB7AoAg7WVgq1J4HsDaQhmYTN0ZggOQ";
 
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",

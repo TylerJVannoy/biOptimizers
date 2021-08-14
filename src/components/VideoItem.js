6a1dad586b147baa95ad2c13638a8f7407d515bf
Tyler Vannoy <61971482+TylerJVannoy@users.noreply.github.com>
2021-08-13 23:30:47 -0700

    fix some typography

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -16,13 +16,13 @@ const VideoItem = ({ video, onVideoSelect }) => {
           alt="Card image cap"
         />
         <div class="card-body">
-          <h5 class="card-title">Card title</h5>
+          <h5 class="card-title">Video Title Here</h5>
           <p class="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
+            Some quick example text to build on the title and make up the bulk
+            of the card's content.
           </p>
           <a href="#" class="btn btn-primary">
-            Go somewhere
+            Watch Video
           </a>
         </div>
       </div>

542fb074a6cecbb1f9e5a7484dfe4346ed61066f
Tyler Vannoy <61971482+TylerJVannoy@users.noreply.github.com>
2021-08-13 18:00:52 -0700

    comment out things

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,32 +1,32 @@
-import React from "react";
-import VideoDetailStyle from "./VideoDetailStyle.css";
+// import React from "react";
+// import VideoDetailStyle from "./VideoDetailStyle.css";
 
-const VideoDetail = ({ video }) => {
-  if (!video) {
-    return <div></div>;
-  }
+// const VideoDetail = ({ video }) => {
+//   if (!video) {
+//     return <div></div>;
+//   }
 
-  const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+//   const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
 
-  return (
-    <div>
-      <img
-        src="https://fb-v1.cdn-bio.com/assets/logo-ahc-5c476361868b32bfadae268911ac58a0.png"
-        className="imagebrand mt-2"
-      ></img>
+//   return (
+//     <div>
+//       <img
+//         src="https://fb-v1.cdn-bio.com/assets/logo-ahc-5c476361868b32bfadae268911ac58a0.png"
+//         className="imagebrand mt-2"
+//       ></img>
 
-      <div className="embed-responsive embed-responsive-16by9 responsive-video mt-2">
-        <iframe
-          className="embed-responsive-item"
-          src={videoSrc}
-          loading="lazy"
-        ></iframe>
-      </div>
-    </div>
-  );
-};
+//       <div className="embed-responsive embed-responsive-16by9 responsive-video mt-2">
+//         <iframe
+//           className="embed-responsive-item"
+//           src={videoSrc}
+//           loading="lazy"
+//         ></iframe>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default VideoDetail;
+// export default VideoDetail;
 
 /*
     <div>

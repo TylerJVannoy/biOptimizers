ece60a4cb24e42aec115d7cc15cb249e7baf1fff
Tyler Vannoy <61971482+TylerJVannoy@users.noreply.github.com>
2021-08-13 20:53:44 -0700

    footer and center

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,63 +4,13 @@ import FooterStyle from "./FooterStyle.css";
 
 export default function Footer() {
   return (
-    <div className=" justify-content-center text-light ">
-      <footer>
-        <div className="row justify-content-center">
-          <div className="col-11">
-            <div className="row ">
-              <div className="col-xl-8 col-md-4 col-sm-4 col-12 my-auto mx-auto a">
-                <h3 className="text-muted mb-md-0 mb-5 bold-text">
-                  BiOptimizers
-                </h3>
-              </div>
-              <div className="col-xl-2 col-md-4 col-sm-4 col-12">
-                <h6 className="mb-3 mb-lg-4 bold-text ">
-                  <b>MENU</b>
-                </h6>
-                <ul className="list-unstyled">
-                  <li>Home</li>
-                  <li>About</li>
-                  <li>Store</li>
-                  <li>Product Support</li>
-                </ul>
-              </div>
-              <div className="col-xl-2 col-md-4 col-sm-4 col-12">
-                <h6 className="mb-3 mb-lg-4 text-muted bold-text mt-sm-0 mt-5">
-                  <b>ADDRESS</b>
-                </h6>
-                <p className="mb-1">5470 Kietzke Lane, Suite 300</p>
-                <p>Reno, NV 89511</p>
-              </div>
-            </div>
-            <div className="row ">
-              <div className="col-xl-8 col-md-4 col-sm-4 col-auto my-md-0 mt-5 order-sm-1 order-3 align-self-end">
-                <small className="rights">
-                  <span>&copy;</span> BiOptimizers USA, Inc. All Rights
-                  Reserved.
-                </small>
-              </div>
-              <div className="col-xl-2 col-md-4 col-sm-4 col-auto order-1 align-self-end ">
-                <h6 className="mt-55 mt-2 text-muted bold-text">
-                  <b>SUPPORT</b>
-                </h6>
-                <small>
-                  {" "}
-                  <span></span> support@bioptimizers.com
-                </small>
-              </div>
-              <div className="col-xl-2 col-md-4 col-sm-4 col-auto order-2 align-self-end ">
-                <h6 className="text-muted bold-text">
-                  <b>TYLER VANNOY</b>
-                </h6>
-                <small>
-                  <span></span> tylervannoy@hotmail.com
-                </small>
-              </div>
-            </div>
-          </div>
-        </div>
-      </footer>
-    </div>
+    <footer class="bg-dark text-center text-lg-start">
+      <div class="text-center text-light p-3">
+        © 2021 Copyright:
+        <a class="text-light" href="https://frontend-portfolio-one.vercel.app/">
+          Tyler Vannoy
+        </a>
+      </div>
+    </footer>
   );
 }
--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -8,7 +8,7 @@ const VideoItem = ({ video, onVideoSelect }) => {
   return (
     <div className="col">
       <div onClick={() => onVideoSelect(video)} className="item video-item">
-        <div className="">
+        <div className="text-center">
           <img
             className=" image"
             width="1296"
